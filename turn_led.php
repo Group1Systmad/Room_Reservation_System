df25c46187f7b2c7472e9b96b32410f82aabd687
Marjay Tapay <tapaymarjay@gmail.com>
2018-03-22 10:55:03 +0800

    ADDED COMMENT

--- turn_led.php
+++ turn_led.php
@@ -9,6 +9,7 @@
         <?php
         
         $count = $_SESSION["num"];
+        //THIS IS A COMMENT
         ?>
         <form method="GET" action="arduino_bridge.php?value=<?php echo ($count%2==0)? 1:0;$_SESSION["num"]++;?>">
             <input type="submit" name="switch_button" value="TURN <?php echo ($count%2==0)? "OFF":"ON";?>">
