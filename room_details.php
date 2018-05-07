4cfd0e5f3758f5520095e3f76b4f557314eb5d41
Marjay Tapay <tapaymarjay@gmail.com>
2018-05-07 21:10:47 +0800

    Added side bars and time function

--- room_details.php
+++ room_details.php
@@ -15,10 +15,10 @@ $rid = $_GET['SID'];
 
 <style>
     .container{
-        width: 100%;
+        width: 85%;
         background: #fff;
         margin: 0 auto;
-        margin-top: 10%;
+        margin-left: 10%;
         padding: 5px;
         border-radius: 10px;
     }
@@ -116,8 +116,89 @@ $rid = $_GET['SID'];
             opacity: 1;
 }
 </style>
+<script type="text/javascript">
+       function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var n= today.getMonth();
+    var o= today.getDate();
+    if (h>12){h= h-12; }
+    h = checkTime(h);
+    m = checkTime(m);
+    n = n+1;
+    n = checkTime(n);
+    o = checkTime(o);
+     document.getElementById('time').innerHTML =
+     h + ":" + m 
+     document.getElementById('date').innerHTML =
+     n + "/" + o 
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}	
+        function logout()
+        {
+	     var confirmdel = confirm("Confirm Log Out?");
+	     if (confirmdel==true)
+	     {
+	     	return true;
+	     }
+	     else
+	     {
+	     	return false;
+	     }
+        }
+   function openaccNav() {
+             document.getElementById("myAccountnav").style.width = "250px";
+             document.getElementById("myAccountnav").style.border = "1px solid black";
+}
+        function closeaccNav() {
+            document.getElementById("myAccountnav").style.width = "0";
+            document.getElementById("myAccountnav").style.border = "none";
+}
+ </script>
 </head>
-<body>
+<body onload="startTime()">
+    
+    
+    <div class="sidebar">
+    <ul>
+        <li> <img src ='logo3.png' style="width: 78%; border-radius: 100%; margin-left: 7px; margin-top: 7px; margin-bottom: 5px"></li> 
+        <li><a onclick="return openaccNav()"><span class="glyphicon glyphicon-user"></span><span class="menu_label">Admin</span></a></li>
+        <li><a href="homepage.php"><span class="glyphicon glyphicon-cloud"></span><span class="menu_label">Home</span></a></li>
+        <li><a href="aboutusadmin.php" ><span class="glyphicon glyphicon-info-sign" ></span><span class="menu_label">About</span></a></li>
+        <li><a href="employees.php"><span class="glyphicon glyphicon-user"></span><span class="menu_label">Accounts</span></a></li>
+        <li><a href="schedtable.php"><span class="glyphicon glyphicon-calendar"></span><span class="menu_label">Reservations</span></a></li>
+        <li><div class="selected"><a href="Room_View.php"><span class="glyphicon glyphicon-blackboard"></span><span class="menu_label">Rooms</span></a></div></li>
+         <li><div id="time" style="padding-top:20px; font-size: 18px; color:white; text-align: center"></div> </li>
+        <li><div id="date" style=" font-size: 12px; color:#ff7a24; text-align: center"></div> </li></ul>
+    </ul>
+    </div>
+       
+        <div id="myAccountnav" class="accnav" style="top:70px;">
+  <a href="javascript:void(0)" class="closebtn hoverable" onclick="closeaccNav()">&times;</a>
+            <?php
+            include 'connect.php';
+            $sql ="select * from accounts where Acc_Uname='".$_SESSION['username']."'";
+            $res = mysqli_query($con, $sql);
+            $row = mysqli_fetch_array($res);
+            $sql1 ="select * from employee where Employee_ID='".$row['Employee_ID']."'";
+            $res1 = mysqli_query($con, $sql1);
+            $row1 = mysqli_fetch_array($res1);
+            ?>
+            <div class="center"> <img src= "<?php  if (empty($row1['Emp_Photo'])){ echo "Male User_96px.png";} else {echo $row1['Emp_Photo'];}?>" style="border-radius: 100%; max-height: 90px;">
+            <div class="name"> <?php echo $row1['Emp_FN']; ?> <?php echo $row1['Emp_LN']; ?> </div>
+            <div class="id"> ID Number: <?php echo $row['Employee_ID']; ?> </div>
+            <hr>
+            <a class="hoverable" href="user_account.php">Account Info</a> 
+            <a  class="hoverable" href="change_pass.php">Change Password</a> 
+            <div class="logoutbtn"> <a class="btn btn-danger" onclick="return logout()" <?php 
+                $_SESSION["login"] = 'logout'; ?> href="login_page.php">Logout</a></div>
+            </div>
+</div>
         <?php
         //code here to get data from database using $rid;
         include 'connect.php';
@@ -129,11 +210,6 @@ $rid = $_GET['SID'];
     
     
       <div class="container">
-          <div class="row">
-              <div class="col-md-3 half">
-                  <a href="Room_View.php"><button class="btn btn-primary">Back</button></a>
-              </div>
-          </div>
             <div class="row">
                 <div class="col-md-6 half">
                 Location
