883ea07a08c405b124d165b4965541968ac8b3b4
Marjay Tapay <tapaymarjay@gmail.com>
2018-05-08 01:46:26 +0800

    Arduino can now send to Database that a Reservation is Finished

--- arduino_bridge.php
+++ arduino_bridge.php
@@ -8,8 +8,21 @@ session_start();
  */
   include "connect.php";
   
+  $pre_mac = $_GET['MAC'];
+  $pre_array_mac = str_split($pre_mac);
+  $count = 0;
+  $post_mac = "";
+  foreach($pre_array_mac as $char){
+      $post_mac = $post_mac . $char;
+      if ($count == 3 || $count == 7){
+          $post_mac = $post_mac . ':';
+      }
+      $count++;
+  }
+  
+  
   $SQL3 = "SELECT tbl_room.room_id,tbl_room.emp_id,tbl_room.time_in,tbl_room.time_out,tbl_room.date,tbl_room.u_code,tbl_room.Status,tbl_room.time_millis,tbl_roomlist.mac_address" 
-      . " FROM tbl_room JOIN tbl_roomlist ON tbl_room.room_id = tbl_roomlist.room_id"; 
+      . " FROM tbl_room JOIN tbl_roomlist ON tbl_room.room_id = tbl_roomlist.room_id WHERE mac_address='$post_mac'"; 
   
      $SQL = "SELECT * FROM tbl_room";
   $result = mysqli_query($con,$SQL3);
--- delsched.php
+++ delsched.php
@@ -2,9 +2,12 @@
 session_start();
 $ID = $_GET['SID'];
 include 'connect.php';
-$SQL = "DELETE FROM tbl_sched WHERE id='$ID'";
-mysqli_query($con,$SQL);
 
+//$SQL = "DELETE FROM tbl_sched WHERE id='$ID'";
+
+
+$SQL2 ="UPDATE tbl_sched SET Status = 0 WHERE id = '$ID'";
+mysqli_query($con,$SQL2);
 mysqli_close($con);
 
 $_SESSION['users'] = false;
